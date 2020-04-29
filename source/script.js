3daccaa20861e8a15af1a762000dd129552c5cbc
Chuck King <cking@surveycto.com>
2020-04-28 23:52:20 -0400

    Allow button label to be set by parameters

--- source/script.js
+++ source/script.js
@@ -3,14 +3,16 @@
 var isAndroid = (document.body.className.indexOf('android-collect') >= 0)
 var smsNumber = getPluginParameter('number')
 var smsMessage = getPluginParameter('message')
+var buttonLabel = getPluginParameter('button_label')
 var previewNumberContainer = document.getElementById('sms-preview-number-container')
 var previewMessageContainer = document.getElementById('sms-preview-message-container')
 var btnSendSMS = document.getElementById('btn-send-sms')
 var statusContainer = document.getElementById('status-container')
 
-// set the preview values to show what has been passed from the parameters
+// Set up the UI with all the values from the parameters.
 previewNumberContainer.innerHTML = smsNumber
 previewMessageContainer.innerHTML = smsMessage
+btnSendSMS.innerText = buttonLabel || 'Send SMS'
 
 // Define what the "Send SMS" button does when the field is not marked readonly.
 if (!fieldProperties.READONLY) {
@@ -25,14 +27,13 @@ if (!fieldProperties.READONLY) {
   btnSendSMS.classList.add('disabled')
 }
 
-
 // The following code sets up and launches the Android intent
 function launchSMSUsingAndroidIntent () {
   // set the parameters for the intent
   var params = {
     uri_data: 'smsto:' + smsNumber,
     sms_body: smsMessage
-  };
+  }
   // Launches the "android.intent.action.SENDTO" intent using the parameters above.
   launchIntent('android.intent.action.SENDTO', params, function (error, result) {
     // Something went wrong while launching the intent.
